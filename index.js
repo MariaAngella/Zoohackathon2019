e7f05ae94d7b31531d6d0708dbe18494418470f4
mariaangella <maria5angella@gmail.com>
2019-11-17 08:45:44 +0300

    final work

--- index.js
+++ index.js
@@ -1,67 +1,59 @@
-
 /* require is a keyword used to use a package and below are the packages */
 const express = require("express");
 const path = require("path");
-const bodyParser = require("body-parser");//middleware-----function that will always run when the path is hit
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const multer = require("multer");
+const mongodb = require("mongodb");
 
 const app = express(); // now we have our express app
+
+// middleware
 app.set("view engine", "pug"); //setting the view engine as pug
 app.set("views", path.join(__dirname, "views")); //there's a folder called views and this is how to get it
 app.use(bodyParser.urlencoded({ extended: true })); //for geting the posted data from the form in the console/body and the output is json
+// app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+var storage = multer.diskStorage({
+  destination: function(req, photo, cb) {
+    cb(null, "uploads");
+  },
+  photoname: function(req, photo, cb) {
+    cb(
+      null,
+      photo,
+      photoname + "-" + Date.now() + path.extname(photo.originalname)
+    );
+  }
+});
+
+var upload = multer({
+  storage: storage
+});
+
+//import routes
+const landingpageRoute = require("./routes/landingpageroute");
+const InformerRoute = require("./routes/informerroute");
+const RangerRoute = require("./routes/rangerroute");
+const RangerFbRoute = require("./routes/rangerfbroute");
+const TouristRoute = require("./routes/touristroute");
+const TouristfbRoute = require("./routes/touristfbroute");
+const AdminRoute = require("./routes/adminroute");
+
+app.use("/", landingpageRoute);
+app.use("/informer", InformerRoute);
+app.use("/ranger", RangerRoute);
+app.use("/rangerfb", RangerFbRoute);
+app.use("/tourist", TouristRoute);
+app.use("/touristfb", TouristfbRoute);
+app.use("/admin", AdminRoute);
 
 /* mongoose db connection */
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/zooahackerthon"); 
-
-/* Creating a Database Schema....schema should be the same format as req.body */
-const registerSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: "Please Enter first name"
-  },
-  lastname: String,
-  gender: String,
-  country: String,
-  city: String
-});
+mongoose.connect("mongodb://localhost:27017/zooahackerthon");
 
 /* creates documents in the db------------------------------------- name of the collection model(collection storage,schemaStructure)....takes on the structure of schema and the req.body(user data) into the Register collection */
-const Informer = mongoose.model("Informer", registerSchema);
-
-/* Routes */
-//Get reads the registerform.pug and displays it on the path
-app.get("/informer", (req, res) => {
-  res.render("informer");
-});
-
-
-
-//extracts all data for the database and displays it
-app.post("/informer", (req, res) => {
-  const informer = new Informer(req.body); //create an instance of the Register model for data entered(req.body==got from the user)
-  informer
-    .save() //and the model is the one saved to the database
-    .then(item => {
-      //.then promise and used because nodejs asyncronously waits
-      Informer.find().then(
-        //query the collection
-        items => {
-          res.render("tourist", { informers: items }); //{users:items}--variable name users---passes all the items to the next page/for i.e list
-        }
-      );
-    })
-    .catch(err => {
-      //.catch promise and used because nodejs asyncronously waits
-      res.status(500).send("unable to save to database");
-    });
-});
-
-
-
-
+//const Register = mongoose.model("Register", registerSchema);
 
 /* listening for requests: the server */
 app.listen(5000, function() {
---
+++ routes/informerroute.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const Register = require("../models/informermodel");
+
+/* Routes */
+//Get reads the registerform.pug and displays it on the path
+router.get("/", (req, res) => {
+  res.render("informer");
+  res.sendFile(__dirname + '/informer')
+});
+
+
+//extracts all data for the database and displays it
+router.post("/", async (req, res) => {
+  const register = new Register(req.body); //create an instance of the Register model for data entered(req.body==got from the user)
+  try {
+    await register.save();
+    console.log("Item has been saved");
+    const items = await Register.find();
+    res.render("informerfb", { informers: items });
+   
+  } catch (err) {
+    //.catch promise and used because nodejs asyncronously waits
+    res.status(500).send("unable to save to database");
+  }
+});
+
+module.exports = router;
